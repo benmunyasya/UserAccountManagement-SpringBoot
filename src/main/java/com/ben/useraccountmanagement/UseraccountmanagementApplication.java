d99da376b1ba41c155f0f8c70b6e3225eecfabd7
benmunyasya <benngovi47@gmail.com>
2025-11-05 12:58:04 +0300

    Initial setup

---
+++ src/main/java/com/ben/useraccountmanagement/UseraccountmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.ben.useraccountmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseraccountmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseraccountmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ben/useraccountmanagement/UseraccountmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ben.useraccountmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseraccountmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
